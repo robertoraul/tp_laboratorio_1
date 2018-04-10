50b577b3dc3d561edb11e58e432b2c3da2039b4f
rcard <robertoraulcardozo@gmail.com>
2018-04-10 01:12:59 -0300

    add files .h and .c

---
+++ funciones.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "funciones.h"
+
+float sumaDosNumeros(float numero1, float numero2){
+    float retorno;
+    retorno = numero1 + numero2;
+
+    return retorno;
+}
+
+float restaDosNumeros(float numero1, float numero2){
+    float retorno;
+    retorno = numero1 - numero2;
+
+    return retorno;
+}
+
+float multiplicaDosNumeros(float numero1, float numero2){
+    float retorno;
+    retorno = numero1 * numero2;
+
+    return retorno;
+}
+
+float dividirDosNumeros(float numero1, float numero2){
+    float retorno;
+
+
+    return retorno;
+}
+
+long factorial(int numero){
+    long retorno;
+
+    return retorno;
+}
---
+++ funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+
+float sumaDosNumeros(float, float);
+float restaDosNumeros(float, float);
+float multiplicaDosNumeros(float, float);
+float dividirDosNumeros(float, float);
+long factorial(int);
+#endif // FUNCIONES_H_INCLUDED
